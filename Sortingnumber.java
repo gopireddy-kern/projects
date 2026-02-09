dfe21cef24505068a1ca30fb72c76ab5b8c008d2
gopireddy-kern <gopi@kernsoftwaresolutions.com>
2026-02-09 17:04:03 +0530

    Add files via upload

---
+++ Sortingnumber.java
@@ -0,0 +1,20 @@
+
+import java.util.Arrays;
+
+
+
+public class Sortingnumber {
+    public static void main (String[] args) {
+        int[] numbers = {5,2,9,1,3};
+
+        Arrays.sort(numbers);
+
+        System.out.println("Sorted numbers:");
+        for (int num : numbers) {
+            System.out.print( num + "");
+
+            }
+
+      }
+    
+}
